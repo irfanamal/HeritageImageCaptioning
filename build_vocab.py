7a68a83f5b95b33583acd35b4023d94d3fade4d2
irfanamal <irfan.ihsanulamal@gmail.com>
2021-10-17 16:20:59 +0700

    Add dataset v3

--- build_vocab.py
+++ build_vocab.py
@@ -3,7 +3,7 @@ import pickle
 import nltk
 from dataset.Vocabulary import Vocabulary
 
-df = pd.read_csv('dataset/data2/train.csv')
+df = pd.read_csv('dataset/data3/train.csv')
 vocab = Vocabulary()
 
 vocab.addWord('<PAD>')
@@ -11,10 +11,11 @@ vocab.addWord('<START>')
 vocab.addWord('<END>')
 vocab.addWord('<UNK>')
 
-for index, caption in df['caption'].iteritems():
-    caption = nltk.word_tokenize(caption.lower())
-    for word in caption:
-        vocab.addWord(word)
+for index, captions in df[['caption1', 'caption2', 'caption3', 'caption4', 'caption5']].iterrows():
+    for idx, caption in captions.iteritems():
+        caption = nltk.word_tokenize(caption.lower())
+        for word in caption:
+            vocab.addWord(word)
 
-with open('dataset/data2/vocab.pkl', 'wb') as f:
+with open('dataset/data3/vocab.pkl', 'wb') as f:
     pickle.dump(vocab, f)
--- dataset/split.py
+++ dataset/split.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import StratifiedShuffleSplit
 
-df = pd.read_csv('dataset/data2/dataset.csv')
+df = pd.read_csv('dataset/data3/dataset.csv')
 x = df.values
 y = df['id_object'].values
 
@@ -11,8 +11,8 @@ for train_index, test_index in train_test_split.split(x,y):
     x_train, x_test = x[train_index], x[test_index]
     y_train, y_test = y[train_index], y[test_index]
 
-test = pd.DataFrame(x_test, columns=['id', 'id_artifact', 'id_object', 'filename', 'article', 'url', 'id_related_image', 'caption'])
-test.to_csv('dataset/data2/test.csv', index=False)
+test = pd.DataFrame(x_test, columns=['id', 'id_artifact', 'id_object', 'filename', 'article', 'url', 'id_related_image', 'caption1', 'caption2', 'caption3', 'caption4', 'caption5'])
+test.to_csv('dataset/data3/test.csv', index=False)
 
 x = x_train.copy()
 y = y_train.copy()
@@ -21,7 +21,7 @@ for train_index, val_index in train_val_split.split(x,y):
     x_train, x_val = x[train_index], x[val_index]
     y_train, y_val = y[train_index], y[val_index]
 
-val = pd.DataFrame(x_val, columns=['id', 'id_artifact', 'id_object', 'filename', 'article', 'url', 'id_related_image', 'caption'])
-val.to_csv('dataset/data2/val.csv', index=False)
-train = pd.DataFrame(x_train, columns=['id', 'id_artifact', 'id_object', 'filename', 'article', 'url', 'id_related_image', 'caption'])
-train.to_csv('dataset/data2/train.csv', index=False)
+val = pd.DataFrame(x_val, columns=['id', 'id_artifact', 'id_object', 'filename', 'article', 'url', 'id_related_image', 'caption1', 'caption2', 'caption3', 'caption4', 'caption5'])
+val.to_csv('dataset/data3/val.csv', index=False)
+train = pd.DataFrame(x_train, columns=['id', 'id_artifact', 'id_object', 'filename', 'article', 'url', 'id_related_image', 'caption1', 'caption2', 'caption3', 'caption4', 'caption5'])
+train.to_csv('dataset/data3/train.csv', index=False)
